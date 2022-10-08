5a0d28c58f88edb265bef0b48520df92be145e4d
Darshan Badiger <darshanabadiger@gmail.com>
2022-10-08 10:47:58 +0530

    Updates

---
+++ precache-manifest.17097711aa82e66790adb48d2592a6a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebbbad60b254f49afe22",
+    "url": "/textutils/static/css/main.d387fa21.chunk.css"
+  },
+  {
+    "revision": "ebbbad60b254f49afe22",
+    "url": "/textutils/static/js/main.ebbbad60.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/textutils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/textutils/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "8c5d3a7ccc996c2dc493",
+    "url": "/textutils/static/js/runtime~main.8c5d3a7c.js"
+  },
+  {
+    "revision": "e0a5de9c8d9347aeb2db0d55721d6f8d",
+    "url": "/textutils/index.html"
+  }
+];
